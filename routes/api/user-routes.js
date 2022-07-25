29765f6f059408c6e83d5879acd3dec31c9553ee
Jonsey <mwjones2186@gmail.com>
2022-07-25 13:43:43 -0700

    password securtiy created. added prover validations within user-routes.

---
+++ routes/api/user-routes.js
@@ -0,0 +1,106 @@
+const router = require('express').Router();
+const {User} = require('../../models');
+
+router.get('/', (req,res)=>{
+    User.findAll()
+        .then(dbUserData => res.json(dbUserData))
+        .catch(error =>{
+            console.log(error);
+            res.status(500).json
+        });
+});
+
+router.get('/:id', (req, res)=>{
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData=>{
+        if (!dbUserData) {
+            res.status(404).json({message: 'No user found with this ID'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(error =>{
+        console.log(error);
+        res.status(500).json(error);
+    });
+});
+
+router.post('/', (req, res)=>{
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData=> res.json(dbUserData))
+    .catch(error =>{
+        console.log(error);
+        res.status(500).json(error)
+    });
+});
+
+router.post('/login', (req, res)=>{
+    user.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData =>{
+        if (!dbUserData) {
+            res.status(400).json({message: 'No user with that email address!'});
+            return;
+        }
+        res.json({user:dbUserData});
+        
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({message: 'Incorrect Password!'});
+            return;
+        }
+        res.json({user: dbUserData, message: 'You are now logged in!'});
+    });
+});
+
+router.put('/:id', (req, res)=>{
+    User.update(req, body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData =>{
+        if (!dbUserDate[0]) {
+            res.status(404).json({message: 'No user found with this ID'});
+            return;
+        }
+        res.json(dbUserData)
+    })
+    .catch(error =>{
+        console.log(error);
+        res.status(500).json(error);
+    });
+});
+
+router.delete('/:id', (req, res)=>{
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData =>{
+        if (!dbUserData) {
+            res.status(404).json({message: 'No user found with this ID'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(error=>{
+        console.log(error);
+        res.status(500).json(error)
+    });
+});
+
+
+module.exports = router;
