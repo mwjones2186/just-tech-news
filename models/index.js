42ee4f932908523e9d0a66591aa947b2a8b7e3cc
Jonsey <mwjones2186@gmail.com>
2022-07-25 17:12:55 -0700

    updated post routes. added info to the index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('/Post');
 
+User.hasMany(Post, {
+    foreignKey: 'user-id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user-id'
+});
+
+
 module.exports = {User, Post};
