fd35bd419884ec44e997c0c558bb60db6ef238c8
Jonsey <mwjones2186@gmail.com>
2022-07-31 11:27:52 -0700

    handlebars installed along with correcting routes and file directory. uploaded seed data.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +14,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(routes);
+app.use(require('./controllers'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 sequelize.sync({force: false}).then(()=>{
     app.listen(PORT,()=> console.log('Now listening'));
